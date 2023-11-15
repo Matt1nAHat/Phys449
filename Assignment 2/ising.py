fdb208a7a1630fea86a593da3e5ca69416028f36
Matthew Charbonneau <47362378+Matt1nAHat@users.noreply.github.com>
2023-11-15 14:29:26 -0800

    Altered Ising.py to actually generate a dataset based on the coupling values given

--- Assignment 2/ising.py
+++ Assignment 2/ising.py
@@ -4,18 +4,28 @@ import numpy as np
 N = 4
 
 # Number of chains
-num_chains = 100
+num_chains = 1000
 
 # Coupler values
-J = np.array([-1, -1, -1, 1])
+J = np.array([-1, 1, 1, 1])
+
+# Calculate the energy of a configuration
+def energy(configuration, J):
+    return -np.sum(J[:-1] * configuration[:-1] * configuration[1:])
 
 # Open the file for writing
-with open('./data/out.txt', 'w') as f:
+with open('./data/inTEST.txt', 'w') as f:
+    # Generate all possible spin configurations for this chain
+    configurations = np.array(np.meshgrid(*[[1, -1] for _ in range(N)])).T.reshape(-1,N)
+
+    # Calculate the probabilities of the configurations
+    probabilities = np.exp(-np.array([energy(c, J) for c in configurations]))
+    probabilities /= np.sum(probabilities)
+
     # Loop over the number of chains
     for _ in range(num_chains):
-        # Generate all possible spin configurations for this chain
-        configurations = np.array(np.meshgrid(*[[1, -1] for _ in range(N)])).T.reshape(-1,N)
+        # Sample a configuration according to these probabilities
+        sampled_configuration = configurations[np.random.choice(range(len(configurations)), p=probabilities)]
 
-        # Write the configurations to the file
-        for configuration in configurations:
-            f.write(''.join(['+' if spin == 1 else '-' for spin in configuration]) + '\n')
+        # Write the sampled configuration to the file
+        f.write(''.join(['+' if spin == 1 else '-' for spin in sampled_configuration]) + '\n')
